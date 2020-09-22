7805f7a0dd76084b39b05661c60b4131967d310d
SheldonDeschamps <70984050+SheldonDeschamps@users.noreply.github.com>
2020-09-22 19:51:39 -0400

    1
    1

--- exercice.py
+++ exercice.py
@@ -3,36 +3,109 @@
 
 
 from typing import List
-
+import sympy
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0:
+        return int(number * -1)
+    elif 0 < number:
+        return abs(number)
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
+    lists = []
+    for i in prefixes:
+        lists.append(i + suffixe)
 
-    return [""]
+    return lists
+
 
 
 def prime_integer_summation() -> int:
-    return 0
+    qt = 0
+    nombre = 2
+    somme = 0
+    while qt < 100:
+        if sympy.isprime(nombre):
+            qt += 1
+            somme += nombre
+        nombre += 1
+    return somme
+
+
+
+
+
+
+
+
+
+
 
 
 def factorial(number: int) -> int:
-    return 0
+    produit = 1
+    while 0 < number:
+        produit = produit * number
+        number -= 1
+    return produit
 
 
 def use_continue() -> None:
-    pass
+    for i in range(1, 11):
+        if i == 5:
+            continue
+        else:
+            print(i)
+
+def age(groupe: List) -> bool:
+    for e in groupe:
+        if e == 25:
+            return True
+
+        elif e < 18:
+            return False
+
+        elif e == 50:
+            for j in groupe:
+                if j == 70:
+                    return False
+                else:
+                    continue
+        elif e == 70:
+            for j in groupe:
+                if j == 50:
+                    return False
+                else:
+                    continue
+    return True
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    accept = []
+    for i in groups:
+        if len(i) < 3 or len(i) > 10:
+            accept.append(False)
+            continue
+        elif age(i):
+            accept.append(True)
+            continue
+        else:
+            accept.append(False)
+
+    return accept
+
+
+
+
+
+
+
 
 
 def main() -> None:
-    number = -4.325
+    number = 4.325
     print(f"La valeur absolue du nombre {number} est {convert_to_absolute(number)}")
 
     print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
